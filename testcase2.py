c18dd9ef97903afb2ad797939320aa31f97ffc05
renukamani <36950900+renukamani@users.noreply.github.com>
2018-05-07 13:19:59 +0530

    Add files via upload

--- testcase2.py
+++ testcase2.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+
 print "1. Run test cases"
 print "2. List test runs"
 print "3.Show test results"
@@ -5,6 +7,7 @@ print "4. generate HTML test reports"
 print "5. Exit"
 option = int(input("enter an option"))
 test_result = list()
+
 if option  == 1 :
     f_num = int(raw_input("enter the number of files u want to run"))
     for i in range(f_num) :
@@ -17,8 +20,6 @@ if option  == 1 :
     test_passed = test_result.count("passed")
     test_failed = test_result.count("failed")
 
-    import xml.etree.cElementTree as ET
-
     root = ET.Element("testresult")
     doc = ET.SubElement(root, "result")
     for i in range(len(test_result)) :
@@ -27,8 +28,37 @@ if option  == 1 :
     ET.SubElement(doc, "testpassed").text = str(test_passed)
     ET.SubElement(doc, "testfailed").text = str(test_failed)
     tree = ET.ElementTree(root)
-    
+
     import datetime
     now = datetime.datetime.now()
     f_name = now.strftime("%Y-%m-%d")+".xml"
     tree.write(f_name)
+
+
+elif option == 2 :
+    import glob
+    files = glob.glob("*.xml")
+    for i in files :
+        print i
+
+elif option == 3:
+    f_name = raw_input("enter the file name")
+    tree = ET.parse(f_name)
+    root = tree.getroot()
+    print "=="*10
+    print root.tag+" "*8+f_name[:-4]
+    print "=="*10
+    for child in root.find("result") :
+        print child.tag,child.text
+    print "=="*10
+
+elif option == 4:
+    import HTML
+    f_name = raw_input("enter the file name")
+    tree = ET.parse(f_name)
+    root = tree.getroot()
+    head = root.tag+" "*8+f_name[:-4]
+    html_list = HTML.List()
+    for child in root.find("result") :
+        html_list.lines.append("%s : %s" % (child.tag,child.text))
+    
